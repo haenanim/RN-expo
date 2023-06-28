c36c26dbd7b8746aaf7192aa6414d64858ffe4d5
haenanim121@gmail.com <haenanim121@gmail.com>
2023-06-28 18:30:23 +0900

    svg  설치

---
+++ AwesomeProject/components/TodoItem.js
@@ -0,0 +1,20 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const TodoItem = () => {
+  return (
+    <View style={styles.itemContainer}>
+      <Pressable hitSlop={10} style={styles.itemTextChecked}>
+        <Text style={[styles.itemText, styles.itemTextChecked]}>코딩하기</Text>
+      </Pressable>
+      <Pressable
+        style={[styles.deleteButton, styles.deleteButtonDone]}
+        hitSlop={10}
+      ></Pressable>
+    </View>
+  );
+};
+
+export default TodoItem;
+
+const styles = StyleSheet.create({});
